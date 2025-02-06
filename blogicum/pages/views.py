06915dd02362ec4e7084225b1af38606a85d112f
Alexey Tyaguschev <lehatop450tav@gmail.com>
2025-02-06 20:16:05 +0300

    updated

---
+++ blogicum/pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def about(request):
+    return render(request, "pages/about.html")
+
+
+def rules(request):
+    return render(request, "pages/rules.html")
+
+# Create your views here.
